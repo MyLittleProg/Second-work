769c1259bc326ea1933e0e8d782fe5f69e00ccb7
AdminAnton <antoxa2102011002@gmail.com>
2023-12-26 23:53:53 +0700

    Update Adaptiv for footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 {
-    
+    //Включения и выключения бургер меню
     
     document.querySelector('.header__burger').addEventListener('click',()=>{
        
@@ -9,8 +9,28 @@
 
         if(document.querySelector(".plashka").classList == 'plashka active-plashka'){
             document.querySelector(".header__menu").style.display = "block";
+            document.body.style.overflow = "hidden";
         }else{
             document.querySelector(".header__menu").style.display = "none";
+            document.body.style.overflow = "scroll";
         }
     })
+
+    //Включения и выключение спойлеров в футоре
+
+        document.querySelector(".wrapper").addEventListener("click", function(event) {
+            const target = event.target;
+            const one_spoyler = document.querySelector('.active-spoiler_mod');
+            
+            if (target.classList.contains("item__title")) {
+                if(one_spoyler != null){
+                    if(one_spoyler.classList.contains("active-spoiler_mod")){
+                        one_spoyler.classList.remove('active-spoiler_mod')
+                       }
+                }
+               target.classList.toggle('active-spoiler_mod')
+            }
+        });
+   
+
 }
